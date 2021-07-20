fee5b0e897ba782aa21c4e80e315b60be2f7a64e
mario <mariobourgeois97.200@gmail.com>
2021-07-20 17:05:00 -0400

    ajout form articletype et categorietype

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,27 +6,37 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
+            ->add('titre', TextType::class)
             ->add('slug')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label' => 'Image'
+            ])
             ->add('image_alt')
-            ->add('contenu', CKEditorType::class)
-            ->add('date_publication')
-            ->add('date_modification')
+            ->add('contenu', CKEditorType::class) // Ce champ sera remplacé par un éditeur WYSIWIG
+            ->add('date_modification', DateType::class, [
+                'widget' => 'single_text',
+            ])
             ->add('estPublie')
             ->add('credit')
-            ->add('miseEnAvant')
+             ->add('miseEnAvant')
             ->add('categories')
-        ;
+            ->add('Enregistrer', SubmitType::class)
+            ->add('Supprimer', SubmitType::class);
     }
 
+    
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
